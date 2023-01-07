7ce20359d48ff978089fe829c35bea37a8ef7363
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-01-07 18:11:00 +0530

    contentful-client

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,23 @@
-export default function Recipes() {
+import { createClient } from "contentful"
+
+export async function getStaticProps() {
+
+  const client = createClient({
+    space: process.env.SPACE_ID,
+    accessToken: process.env.ACCESS_TOKEN
+  });
+
+  const res = await client.getEntries({ content_type: 'recipe' });
+
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
   return (
     <div className="recipe-list">
       Recipe List
