ae8e7b60b74f51f1a593e76aa40793bd57408854
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-01-07 19:42:04 +0530

    styled jsx

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,13 @@ export default function Recipes({ recipes }) {
       {recipes.map(recipe => (
         <RecipeCard key={recipe.sys.id} recipe={recipe} />
       ))}
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          gap: 20px 60px;
+        }
+      `}</style>
     </div>
   )
 }
